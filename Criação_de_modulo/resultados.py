a5b04e18c0924e3f043206c0c77ca2ad383dcce4
Cesar Netto <83458311+cesarnetto@users.noreply.github.com>
2024-12-20 00:29:28 -0300

    uploud de arquivos

---
+++ Criação_de_modulo/resultados.py
@@ -0,0 +1,4 @@
+import sorteio
+
+ganhador = sorteio.escolher_ganhador ("estevao", "cesar", "bruno", "carla")
+print(ganhador)
---
+++ Criação_de_modulo/sorteio.py
@@ -0,0 +1,5 @@
+import random
+
+def escolher_ganhador (*participantes):
+    return random.choice (participantes)
+    
